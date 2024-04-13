8770ba63c6f0aa58fa0d73fe3d3e92fb76042270
JosephQV <112574271+JosephQV@users.noreply.github.com>
2024-04-13 13:36:33 -0400

    Update IRTapeBotMain.ino

--- IRTapeBotMain/IRTapeBotMain.ino
+++ IRTapeBotMain/IRTapeBotMain.ino
@@ -1,9 +1,37 @@
+const int LEFTSENSEPIN = 45;
+const int RIGHTSENSEPIN = 41;
+const int STOPSENSEPIN = 37;
+
+const int STOPMOTORPIN = 33;
+const int STEERINGLEFTPIN = 2;
+const int STEERINGRIGHTPIN = 3;
+
 void setup() {
-  // put your setup code here, to run once:
+  //Set input pins for IR sensors
+  //2 IR Sensors at front for steering detection
+  pinMode(LEFTSENSEPIN, INPUT);
+  pinMode(RIGHTSENSEPIN, INPUT);
+  //1 IR Sensor at side for stop detection
+  pinMode(STOPSENSEPIN, INPUT);
+  //set output pin for start/stopping back wheel motors
+  pinMode(STOPMOTORPIN, OUTPUT);
+  //set digital output pins for controlling left and right steering
+  pinMode(STEERINGLEFTPIN, OUTPUT);
+  pinMode(STEERINGRIGHTPIN, OUTPUT);
   
+  //Serial monitor for printing for testing
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  //Take input from front IR sensors
+  int LEFTreading = digitalRead(LEFTSENSEPIN);
+  int RIGHTreading = digitalRead(RIGHTSENSEPIN);
+  //take input from side IR sensor
+  int STOPreading = digitalRead(STOPSENSEPIN);
+  //TESTING - print inputs
+  Serial.println(LEFTreading);
+  Serial.println(RIGHTreading);
+  Serial.println(STOPreading);
+  delay(300);
 }
