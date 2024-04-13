fa74abe35b68c7219b2a3321c1ded85d5e67adb9
Christopher Matthews <136629267+Spider-boy19@users.noreply.github.com>
2024-04-13 13:11:25 -0400

    Update IRTapeBotMain.ino

--- IRTapeBotMain/IRTapeBotMain.ino
+++ IRTapeBotMain/IRTapeBotMain.ino
@@ -5,5 +5,23 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  //Check with both Sencer if on line
 
+  // if both sencers is on line{
+      // Move Foward
+      // Check if there is a stop sign
+      // if there is a stop sign{
+        // stop for certan time
+        // Move Forward
+  
+  // if Left sencer is high{
+    // Turn left
+    // Move foward
+
+  // if Right Sencer is high{
+    // Turn Right
+    // Move forward
+
+  // if both sencers are high{
+    // Edit
 }
